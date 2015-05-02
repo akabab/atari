e3a7dc1ad90428bcf510553b9aedcf930db64b59
ycribier <ycribier@student.42.fr>
2015-05-02 17:17:51 +0200

    clean

--- srcs/main.c
+++ srcs/main.c
@@ -51,15 +51,20 @@ int main(void)
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
+
         glViewport(0, 0, width, height);
         //clear screen
         glClear(GL_COLOR_BUFFER_BIT);
+
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         glMatrixMode(GL_MODELVIEW);
+
         glLoadIdentity();
         glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+        //draw triangle
         glBegin(GL_TRIANGLES);
         glColor3f(1.f, 0.f, 0.f);
         glVertex3f(-0.6f, -0.4f, 0.f);
