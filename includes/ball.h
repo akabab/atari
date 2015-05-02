a3bf0f007e01ebd86b5b52704133dd3b790c4830
tdieumeg <tdieumeg@student.42.fr>
2015-05-02 21:16:47 +0200

    ~~

--- includes/ball.h
+++ includes/ball.h
@@ -6,7 +6,7 @@
 # define BALL_RADIUS 0.03f
 # define SPEED_STEP 0.01f
 # define SPEED_MODIFIER 5
-# define BALL_ORIGIN_SPEED ( * SPEED_STEP)
+# define BALL_ORIGIN_SPEED (SPEED_MODIFIER * SPEED_STEP)
 # define BALL_MVT_ANGLE (5 * M_PI / 6)
 
 typedef struct		s_ball
